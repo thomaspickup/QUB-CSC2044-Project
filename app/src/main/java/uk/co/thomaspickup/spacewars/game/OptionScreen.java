46b7abf654575997ca2ac58f0249632b30ee6a88
Thomas Pickup <hello@thomaspickup.co.uk>
2018-03-09 17:40:24 +0000

    Added Class for option screen

---
+++ app/src/main/java/uk/co/thomaspickup/spacewars/game/OptionScreen.java
@@ -0,0 +1,56 @@
+package uk.co.thomaspickup.spacewars.game;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Rect;
+
+import java.util.List;
+
+import uk.co.thomaspickup.spacewars.gage.Game;
+import uk.co.thomaspickup.spacewars.gage.engine.AssetStore;
+import uk.co.thomaspickup.spacewars.gage.engine.ElapsedTime;
+import uk.co.thomaspickup.spacewars.gage.engine.graphics.IGraphics2D;
+import uk.co.thomaspickup.spacewars.gage.engine.input.Input;
+import uk.co.thomaspickup.spacewars.gage.engine.input.TouchEvent;
+import uk.co.thomaspickup.spacewars.gage.world.GameScreen;
+import uk.co.thomaspickup.spacewars.game.spaceLevel.SpaceLevelScreen;
+
+/**
+ * Created by Thomas Pickup on 09/03/2018.
+ */
+
+public class OptionScreen extends GameScreen {
+    /**
+     * Create a simple options screen
+     *
+     * @param game
+     *            SpaceGame to which this screen belongs
+     */
+    public OptionScreen(Game game) {
+        super("OptionScreen", game);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * uk.ac.qub.eeecs.gage.world.GameScreen#update(uk.ac.qub.eeecs.gage.engine
+     * .ElapsedTime)
+     */
+    @Override
+    public void update(ElapsedTime elapsedTime) {
+
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * uk.ac.qub.eeecs.gage.world.GameScreen#draw(uk.ac.qub.eeecs.gage.engine
+     * .ElapsedTime, uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D)
+     */
+    @Override
+    public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
+
+    }
+}
