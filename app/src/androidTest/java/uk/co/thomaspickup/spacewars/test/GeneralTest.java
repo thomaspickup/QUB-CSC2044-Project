1b8fe60cab5a847e78c5416fd52b691ab58a6e2e
Thomas Pickup <hello@thomaspickup.co.uk>
2018-04-08 01:01:26 +0100

    created test classes for various functions

---
+++ app/src/androidTest/java/uk/co/thomaspickup/spacewars/test/GeneralTest.java
@@ -0,0 +1,53 @@
+package uk.co.thomaspickup.spacewars.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.co.thomaspickup.spacewars.game.HelperTools;
+
+/**
+ * Created by thomaspickup on 08/04/2018.
+ */
+
+public class GeneralTest {
+    private HelperTools helperTools = new HelperTools();
+
+    // Helper Tools Tests
+    @Test
+    public void testHelperToolsInBetween() {
+        // Expected: True 2 is in between 1 & 4
+        boolean expectedResult = true;
+        Assert.assertEquals(expectedResult, helperTools.inBetween(2,1,4));
+
+        // Expected: False 400 is lower than the lower bound
+        expectedResult = false;
+        Assert.assertEquals(expectedResult, helperTools.inBetween(400, 500,600));
+    }
+
+    @Test
+    public void testHelperToolsSpeedMultiplier() {
+        // Expected: Returns the default value of 1.0f
+        float expectedResult = 1.0f;
+        Assert.assertEquals(expectedResult, helperTools.getSpeedMultiplier(500),0.0);
+
+        // Expected: Returns the corresponding value of 1.5f
+        expectedResult = 1.5f;
+        Assert.assertEquals(expectedResult, helperTools.getSpeedMultiplier(3),0.0);
+    }
+
+    @Test
+    public void testHelperToolsGetDistance() {
+        // Expected: 7.07f (Used Calculator)
+        float deltaX = 5.0f;
+        float deltaY = 5.0f;
+        float expectedResult = 7.07f;
+        Assert.assertEquals(expectedResult, helperTools.getDistance(deltaX,deltaY), 0.1);
+
+        // Expected: 471.7f (Used Calculator)
+        deltaX = 250f;
+        deltaY = 400f;
+        expectedResult = 471.7f;
+        Assert.assertEquals(expectedResult, helperTools.getDistance(deltaX,deltaY), 0.1);
+    }
+}
---
+++ app/src/androidTest/java/uk/co/thomaspickup/spacewars/test/PlayerTest.java
@@ -0,0 +1,61 @@
+package uk.co.thomaspickup.spacewars.test;
+
+import android.content.Context;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import uk.co.thomaspickup.spacewars.game.SettingsHandler;
+import uk.co.thomaspickup.spacewars.game.SpaceGame;
+import uk.co.thomaspickup.spacewars.game.spaceLevel.PlayerSpaceship;
+
+import static android.support.test.InstrumentationRegistry.getContext;
+
+/**
+ * Created by thomaspickup on 07/04/2018.
+ */
+
+public class PlayerTest {
+    private Context testContext;
+    private SettingsHandler settingsHandler;
+    private PlayerSpaceship playerSpaceship;
+    private SpaceGame spaceGame;
+
+    public PlayerTest() {
+        super();
+    }
+
+    @Before
+    public void preTest() {
+        // Creates a test gamescreen
+        spaceGame = new SpaceGame();
+
+        // Gets the context for the test runner
+        testContext = getContext();
+
+        // Creates a new instance of settingsHandler
+        settingsHandler = new SettingsHandler();
+    }
+
+    // Tests that the settings handler can store and get settings correctly
+    @Test
+    public void testSettingsSetGet() {
+        // Sets sound as 1 expects to get 1
+        settingsHandler.setSound(testContext, 1);
+        int expectedResult = 1;
+
+        // Tests to see if recovery of setting is correct
+        Assert.assertEquals(expectedResult, settingsHandler.getSound(testContext));
+
+        // Sets difficulty as 4 expects to get 4
+        settingsHandler.setDifficulty(testContext, 2);
+        expectedResult = 2;
+
+        // Tests to see if recovery of setting is correct
+        Assert.assertEquals(expectedResult, settingsHandler.getDifficulty(testContext));
+    }
+
+
+}
