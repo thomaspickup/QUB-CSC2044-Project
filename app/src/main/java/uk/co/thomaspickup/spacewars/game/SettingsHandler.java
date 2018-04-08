41d96e0f293bef81b4a709bde0227c44da448d97
Thomas Pickup <hello@thomaspickup.co.uk>
2018-04-08 22:20:38 +0100

    Finished annotating SettingsHandler

--- app/src/main/java/uk/co/thomaspickup/spacewars/game/SettingsHandler.java
+++ app/src/main/java/uk/co/thomaspickup/spacewars/game/SettingsHandler.java
@@ -1,190 +1,212 @@
 package uk.co.thomaspickup.spacewars.game;
 
+// /////////////////////////////////////////////////////////////////////////
+// Imports
+// /////////////////////////////////////////////////////////////////////////
+
 import android.content.Context;
 import android.content.SharedPreferences;
 
 /**
- * This class handles all the settingsHandler for the app, both setting them and getting them.
- * Created by thomaspickup.
+ * This class handles the setting and getting of the SharedPreferences.
+ * 
+ * Created by Thomas Pickup
  */
-
-// TODO: Annotate
 public class SettingsHandler {
+    // /////////////////////////////////////////////////////////////////////////
+    // Variables
+    // /////////////////////////////////////////////////////////////////////////
+
     // Sets up a new instance of helper tools
     // for commonly used algorithms.
     HelperTools maths = new HelperTools();
 
-    /**
-     * Constructor for the SettingsHandler class.
-     * Allows for class to be implemented easily.
-     */
-    public SettingsHandler() {
-        // Does nothing when initializing.
-    }
+    // /////////////////////////////////////////////////////////////////////////
+    // Methods
+    // /////////////////////////////////////////////////////////////////////////
 
     /**
-     *  This function returns the difficulty setting that is stored in sharedPreferences.
-     *  Difficulty Settings:
-     *  Easy - 1
-     *  Normal - 2
-     *  Hard - 3
-     *  Insane - 4
+     * This function returns the difficulty setting that is stored in sharedPreferences.
+     * Difficulty Settings:
+     * Easy - 1
+     * Normal - 2
+     * Hard - 3
+     * Insane - 4
      *
-      * @param appContext - Passes the Context of the application to allow for accessing system level functions.
+     * @param appContext - Passes the Context of the application to allow for accessing system level functions.
      * @return The difficulty stored in SharedPreferences.
      */
     public int getDifficulty(Context appContext) {
+        // Integer value to hold setting
         int difficultySetting;
+
+        // Transfers the appContext
         Context myAppContext = appContext;
 
-        // Creates instance of SharePreferences called settingsHandler
+        // Creates instance of SharedPreferences called settings
         SharedPreferences settings = myAppContext.getSharedPreferences(AppStrings.getPREF_(), 0);
 
         // Creates instance of SharedPreferences Editor called editor
         SharedPreferences.Editor editor;
 
+        // Checks to see if settings contains a Difficulty setting
         if (!settings.contains(AppStrings.getPREF_DIFFICULTY())) {
-            // If the key doesn't already exist then create the default key of 0
-
-            // Defines the editor as the edit of settingsHandler
+            // If it doesn't contain the setting already then we create one and set it as the default (2)
+            // Defines the editor as the edit of settings
             editor = settings.edit();
 
-            // Puts the int
+            // Puts the integer 2 into the editor
             editor.putInt(AppStrings.getPREF_DIFFICULTY(), 2);
 
-            // Commits changes to shared preferences
+            // Commits changes to settings (SharedPreferences)
             editor.commit();
 
+            // Gets the difficulty setting from Shared Prefences
             difficultySetting = settings.getInt(AppStrings.getPREF_DIFFICULTY(), 2);
         } else {
-            // If the key has already been set return the value stored in the key
+            // If the setting has already been set return the value stored in the setting
             difficultySetting = settings.getInt(AppStrings.getPREF_DIFFICULTY(), 2);
         }
 
-        // Returns the result from SharedPreferences
+        // Returns the value
         return difficultySetting;
     }
 
     /**
-     *  This function sets the difficulty setting to be stored in sharedPreferences.
-     *  Difficulty Settings:
-     *  Easy - 1
-     *  Normal - 2
-     *  Hard - 3
-     *  Insane - 4
-     *
-     * @param appContext - Passes the Context of the application to allow for accessing system level functions.
-     * @param difficultySetting - Passes through the difficulty setting.
-     * @return Success (True or False)
-     */
-    public boolean setDifficulty(Context appContext, int difficultySetting) {
-        if (maths.inBetween(difficultySetting, 1, 4)) {
-            int myDifficultySetting = difficultySetting;
-            Context myAppContext = appContext;
-
-            // Creates instance of SharePreferences called settingsHandler
-            SharedPreferences settings = myAppContext.getSharedPreferences(AppStrings.getPREF_(), 0);
-
-            // Creates instance of SharedPreferences Editor called editor
-            SharedPreferences.Editor editor;
-
-            // Defines the editor as the edit of settingsHandler
-            editor = settings.edit();
-
-            // Puts the int
-            editor.putInt(AppStrings.getPREF_DIFFICULTY(), myDifficultySetting);
-
-            // Commits changes to shared preferences
-            editor.commit();
-
-            if (getDifficulty(myAppContext) == myDifficultySetting) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     *  This function sets the sound setting to be stored in sharedPreferences.
-     *  Difficulty Settings:
-     *  Mute - 0
-     *  Un-Mute - 1
-     *
-     * @param appContext - Passes the Context of the application to allow for accessing system level functions.
-     * @param soundSetting - Passes through the sound setting.
-     * @return Success (True or False)
-     */
-    public boolean setSound(Context appContext, int soundSetting) {
-        if (maths.inBetween(soundSetting, 0, 1)) {
-            int mySoundSetting = soundSetting;
-            Context myAppContext = appContext;
-
-            // Creates instance of SharePreferences called settingsHandler
-            SharedPreferences settings = myAppContext.getSharedPreferences(AppStrings.getPREF_(), 0);
-
-            // Creates instance of SharedPreferences Editor called editor
-            SharedPreferences.Editor editor;
-
-            // Defines the editor as the edit of settingsHandler
-            editor = settings.edit();
-
-            // Puts the int
-            editor.putInt(AppStrings.getPREF_SOUND(), mySoundSetting);
-
-            // Commits changes to shared preferences
-            editor.commit();
-
-            if (getSound(myAppContext) == mySoundSetting) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     *  This function returns the sound setting that is stored in sharedPreferences.
-     *  Difficulty Settings:
-     *  Mute - 0
-     *  Un-Mute - 1
+     * This function returns the sound setting that is stored in sharedPreferences.
+     * Difficulty Settings:
+     * Mute - 0
+     * Un-Mute - 1
      *
      * @param appContext - Passes the Context of the application to allow for accessing system level functions.
      * @return The difficulty stored in SharedPreferences.
      */
     public int getSound(Context appContext) {
+        // Integer value to hold the setting
         int soundSetting;
+
+        // Transfers the appContext
         Context myAppContext = appContext;
 
-        // Creates instance of SharePreferences called settingsHandler
+        // Creates instance of SharedPreferences called settings
         SharedPreferences settings = myAppContext.getSharedPreferences(AppStrings.getPREF_(), 0);
 
         // Creates instance of SharedPreferences Editor called editor
         SharedPreferences.Editor editor;
 
         if (!settings.contains(AppStrings.getPREF_SOUND())) {
-            // If the key doesn't already exist then create the default key value of 0
-
-            // Defines the editor as the edit of settingsHandler
+            // If it doesn't contain the setting already then we create one and set it as the default (1)
+            // Defines the editor as the edit of settings
             editor = settings.edit();
 
-            // Puts the int
-            editor.putInt(AppStrings.getPREF_DIFFICULTY(), 0);
+            // Puts the integer 1 into the editor
+            editor.putInt(AppStrings.getPREF_DIFFICULTY(), 1);
 
-            // Commits changes to shared preferences
+            // Commits changes to settings (SharedPreferences)
             editor.commit();
 
+            // Gets the sound setting from Shared Prefences
             soundSetting = settings.getInt(AppStrings.getPREF_SOUND(), 1);
         } else {
-            // If the key has already been set return the value stored in the key
+            // If the setting has already been set return the value stored in the setting
             soundSetting = settings.getInt(AppStrings.getPREF_SOUND(), 1);
         }
 
-        // Returns the result from SharedPreferences
+        // Returns the value
         return soundSetting;
+    }
+
+    /**
+     * This function sets the difficulty setting to be stored in sharedPreferences.
+     * Difficulty Settings:
+     * Easy - 1
+     * Normal - 2
+     * Hard - 3
+     * Insane - 4
+     *
+     * @param appContext        - Passes the Context of the application to allow for accessing system level functions.
+     * @param difficultySetting - Passes through the difficulty setting.
+     * @return Success (True or False)
+     */
+    public boolean setDifficulty(Context appContext, int difficultySetting) {
+        // If the setting (as integer) passed is in between 1 and 4 then we continue
+        if (maths.inBetween(difficultySetting, 1, 4)) {
+            // Transfers the values passed into variables
+            int myDifficultySetting = difficultySetting;
+            Context myAppContext = appContext;
+
+            // Creates instance of SharedPreferences called settings
+            SharedPreferences settings = myAppContext.getSharedPreferences(AppStrings.getPREF_(), 0);
+
+            // Creates instance of SharedPreferences Editor called editor
+            SharedPreferences.Editor editor;
+
+            // Defines the editor as the edit of settings
+            editor = settings.edit();
+
+            // Puts the integer that was passed into the editor
+            editor.putInt(AppStrings.getPREF_DIFFICULTY(), myDifficultySetting);
+
+            // Commits changes to settings (SharedPreferences)
+            editor.commit();
+
+            // Tests that everything has gone through okay by fetching and comparing
+            if (getDifficulty(myAppContext) == myDifficultySetting) {
+                // Setting has been stored correctly
+                return true;
+            } else {
+                // Setting has been stored incorrectly
+                return false;
+            }
+        } else {
+            // Setting (integer) passed was not in between 1 & 4
+            return false;
+        }
+    }
+
+    /**
+     * This function sets the sound setting to be stored in sharedPreferences.
+     * Difficulty Settings:
+     * Mute - 0
+     * Un-Mute - 1
+     *
+     * @param appContext   - Passes the Context of the application to allow for accessing system level functions.
+     * @param soundSetting - Passes through the sound setting.
+     * @return Success (True or False)
+     */
+    public boolean setSound(Context appContext, int soundSetting) {
+        // If the setting (as integer) passed is in between 0 and 1 then we continue
+        if (maths.inBetween(soundSetting, 0, 1)) {
+            // Transfers the values passed into variables
+            int mySoundSetting = soundSetting;
+            Context myAppContext = appContext;
+
+            // Creates instance of SharedPreferences called settings
+            SharedPreferences settings = myAppContext.getSharedPreferences(AppStrings.getPREF_(), 0);
+
+            // Creates instance of SharedPreferences Editor called editor
+            SharedPreferences.Editor editor;
+
+            // Defines the editor as the edit of setting
+            editor = settings.edit();
+
+            // Puts the integer that was passed into the editor
+            editor.putInt(AppStrings.getPREF_SOUND(), mySoundSetting);
+
+            // Commits changes to settings (SharedPreferences)
+            editor.commit();
+
+            // Tests that everything has gone through okay by fetching and comparing
+            if (getSound(myAppContext) == mySoundSetting) {
+                // Setting has been stored correctly
+                return true;
+            } else {
+                // Setting has been stored incorrectly
+                return false;
+            }
+        } else {
+            // Setting (integer) passed was not in between 0 & 1
+            return false;
+        }
     }
 }
