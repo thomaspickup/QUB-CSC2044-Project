d1af2a6dee608f29656b9269438bd9b297f27992
Thomas Pickup <hello@thomaspickup.co.uk>
2018-04-12 14:00:21 +0100

    Finsihed optimizing and annotating Laser.Java

--- app/src/main/java/uk/co/thomaspickup/spacewars/game/spaceLevel/Laser.java
+++ app/src/main/java/uk/co/thomaspickup/spacewars/game/spaceLevel/Laser.java
@@ -1,8 +1,11 @@
 package uk.co.thomaspickup.spacewars.game.spaceLevel;
 
+// /////////////////////////////////////////////////////////////////////////
+// Imports
+// /////////////////////////////////////////////////////////////////////////
+
 import android.graphics.Bitmap;
 
-import uk.co.thomaspickup.spacewars.gage.Game;
 import uk.co.thomaspickup.spacewars.gage.engine.ElapsedTime;
 import uk.co.thomaspickup.spacewars.gage.engine.graphics.IGraphics2D;
 import uk.co.thomaspickup.spacewars.gage.util.Vector2;
@@ -12,12 +15,18 @@ import uk.co.thomaspickup.spacewars.gage.world.ScreenViewport;
 import uk.co.thomaspickup.spacewars.gage.world.Sprite;
 
 /**
- * Created by thomaspickup on 03/04/2018.
+ * Class used to represent a Laser launched from a sprite
+ *
+ * Created by Thomas Pickup
  */
-// TODO: Optimize and annotate
 public class Laser extends Sprite {
-    // ints to hold positioning
-    private int startX, startY, endX, endY;
+    // /////////////////////////////////////////////////////////////////////////
+    // Variables
+    // /////////////////////////////////////////////////////////////////////////
+
+    // /////////////////////////////////////////////////////////////////////////
+    // Constructor
+    // /////////////////////////////////////////////////////////////////////////
 
     /**
      * Constructor for the laser object
@@ -29,17 +38,29 @@ public class Laser extends Sprite {
     public Laser(int startX, int startY, GameScreen gameScreen, Bitmap sprite, Vector2 playerAcceleration, Vector2 playerVelocity, float orientation) {
         super(startX,startY, 9, 3, sprite, gameScreen);
 
-        acceleration.x = playerAcceleration.x + 2;
-        acceleration.y = playerAcceleration.y + 2;
-        velocity.x = playerVelocity.x + 2;
-        velocity.y = playerVelocity.y + 2;
+        // Sets acceleration and velocity to be player acceleration and velocity * 2
+        acceleration.x = playerAcceleration.x * 2;
+        acceleration.y = playerAcceleration.y * 2;
+        velocity.x = playerVelocity.x * 2;
+        velocity.y = playerVelocity.y * 2;
 
+        // Sets orientation to be the same as the player
         this.orientation = orientation;
 
+        // Sets the max acceleration and velocity
         maxAcceleration = 300.0f;
         maxVelocity = 100.0f;
     }
 
+    // /////////////////////////////////////////////////////////////////////////
+    // Update and Draw Methods
+    // /////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Updates the Laser.
+     *
+     * @param elapsedTime
+     */
     @Override
     public void update(ElapsedTime elapsedTime) {
         float dt = (float) elapsedTime.stepTime;
@@ -63,6 +84,14 @@ public class Laser extends Sprite {
         position.add(velocity.x *dt , velocity.y * dt);
     }
 
+    /**
+     * Draws the Laser to Screen
+     *
+     * @param elapsedTime
+     * @param graphics2D
+     * @param mLayerViewport
+     * @param mScreenViewport
+     */
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D, LayerViewport mLayerViewport, ScreenViewport mScreenViewport) {
         super.draw(elapsedTime, graphics2D, mLayerViewport, mScreenViewport);
